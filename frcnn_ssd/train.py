8c49037dca81ee3ba8f6c419a62ee385f5ebdeac
Simba-brother <2196664077@qq.com>
2026-01-04 20:36:49 +0800

    go

--- frcnn_ssd/train.py
+++ frcnn_ssd/train.py
@@ -358,7 +358,7 @@ def collection_FRCNN_indicator(model,device,dataloader,epoch):
 if __name__ == "__main__":
 
     exp_data_root_dir = "/data/mml/data_debugging_data"
-    dataset_name = "VOC2012" # VOC2012|KITTI_8|VisDrone
+    dataset_name = "KITTI_8" # VOC2012|KITTI_8|VisDrone
     model_name = "FRCNN" # FRCNN|SSD
     trainset_status = "repair_datactive" # clean|error|repair_datactive|repair_ours
 
@@ -368,7 +368,7 @@ if __name__ == "__main__":
     else:
         model_weight_path = None
         num_epochs = 50
-    gpu_id = 1
+    gpu_id = 0
     init_lr = 5e-3  # SSD:1e-3
     epoch_save_dir = os.path.join(exp_data_root_dir,"models",dataset_name.lower(),
                                   model_name.lower(), trainset_status)
