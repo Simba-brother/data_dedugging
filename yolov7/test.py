9768d8cc81d571e384aaf5bb524e4ffdbd8bf10c
Simba-brother <2196664077@qq.com>
2026-03-10 13:48:16 +0800

    go

--- yolov7/test.py
+++ yolov7/test.py
@@ -359,7 +359,7 @@ if __name__ == '__main__':
     gpu_id = 0
     parser = argparse.ArgumentParser(prog='test.py')
     parser.add_argument('--weights', nargs='+', type=str, 
-                        default=f'trained_models/{dataset_name.lower()}/clean.pt', help='model.pt path(s)')
+                        default=f'trained_models/{dataset_name.lower()}/margin_best.pt', help='model.pt path(s)')
     parser.add_argument('--data', type=str, default=f'data/{dataset_name}.yaml', help='*.data path')
     parser.add_argument('--batch-size', type=int, default=32, help='size of each image batch')
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
